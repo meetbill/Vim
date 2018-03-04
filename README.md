8b6e3e39651eadf193a1e17d1b753da64a78a5fa
meetbill <meetbill@163.com>
2018-03-04 16:30:27 +0800

    适配 Mac 安装

--- README.md
+++ README.md
@@ -14,9 +14,9 @@
 ```
 
 <!-- vim-markdown-toc GFM -->
-* [1 vim IDE 部署](#1-vim-ide-部署)
-    * [1.1 git 进行下载安装](#11-git-进行下载安装)
-    * [1.2 curl 进行下载安装](#12-curl-进行下载安装)
+* [1 Vim IDE 部署](#1-vim-ide-部署)
+    * [1.1 Git 进行下载安装](#11-git-进行下载安装)
+    * [1.2 Curl 进行下载安装](#12-curl-进行下载安装)
     * [1.3 卸载](#13-卸载)
 * [2 牛刀小试](#2-牛刀小试)
 * [3 版本发布](#3-版本发布)
@@ -24,12 +24,12 @@
 
 <!-- vim-markdown-toc -->
 
-## 1 vim IDE 部署
+## 1 Vim IDE 部署
 
 > * 本 Vim 可以重复安装
 > * 普通用户执行程序时需要有 sudo 权限
 
-### 1.1 git 进行下载安装
+### 1.1 Git 进行下载安装
 ```
 #git clone https://github.com/BillWang139967/Vim.git
 #chmod 777 -R Vim
@@ -38,7 +38,7 @@
 ```
 **常见问题**
 
-使用 git clone 失败
+使用 Git clone 失败
 
 ```
 [root@localhost ~]# git clone https://github.com/BillWang139967/Vim.git
@@ -51,7 +51,7 @@ fatal: HTTP request failed
 ```
 #git config --global http.sslVerify false
 ```
-### 1.2 curl 进行下载安装
+### 1.2 Curl 进行下载安装
 
 ```
 #curl -o vim.zip https://codeload.github.com/BillWang139967/Vim/zip/master
@@ -77,17 +77,17 @@ fatal: HTTP request failed
 
 ## 3 版本发布
 
-* [v1.0.12]  2017-09-28  增加：golang 语法高亮
-* [v1.0.11]  2017-07-24  修改：状态栏显示
-* [v1.0.10]  2017-04-21  增加：python 代码自动审查(pyflakes)
-* [v1.0.09]  2017-04-18  增加：括号自动匹配插件 [auto-pairs](https://github.com/jiangmiao/auto-pairs)
-* [v1.0.08]  2017-03-18  增加：自动格式化中文排版插件 [pangu](https://github.com/hotoo/pangu.vim), 更新 [TOC](https://github.com/mzlogin/vim-markdown-toc) 插件。
-* [v1.0.07]  2016-10-07  增加：Markdown 生成 [TOC](https://github.com/mzlogin/vim-markdown-toc) 插件
-* [v1.0.05]  2016-08-26  修改：更新 ctags, 添加 python 代码自动折叠
-* [v1.0.04]  2016-05-29  增加：自动生成 markdown 头信息
-* [v1.0.03]  2016-05-18  修订：对 github 文档进行整理
-* [v1.0.02]  2014-08-13  新增：对 Vim 进行整理发布一键化程序
-* [v1.0.01]  2014-01-01  新增：发布初始版本。
+* [V1.0.12]  2017-09-28  增加：Golang 语法高亮
+* [V1.0.11]  2017-07-24  修改：状态栏显示
+* [V1.0.10]  2017-04-21  增加：Python 代码自动审查(pyflakes)
+* [V1.0.09]  2017-04-18  增加：括号自动匹配插件 [auto-pairs](https://github.com/jiangmiao/auto-pairs)
+* [V1.0.08]  2017-03-18  增加：自动格式化中文排版插件 [pangu](https://github.com/hotoo/pangu.vim), 更新 [TOC](https://github.com/mzlogin/vim-markdown-toc) 插件。
+* [V1.0.07]  2016-10-07  增加：Markdown 生成 [TOC](https://github.com/mzlogin/vim-markdown-toc) 插件
+* [V1.0.05]  2016-08-26  修改：更新 Ctags, 添加 Python 代码自动折叠
+* [V1.0.04]  2016-05-29  增加：自动生成 markdown 头信息
+* [V1.0.03]  2016-05-18  修订：对 GitHub 文档进行整理
+* [V1.0.02]  2014-08-13  新增：对 Vim 进行整理发布一键化程序
+* [V1.0.01]  2014-01-01  新增：发布初始版本。
 
 ## 4 小额捐款
 
--- start_vim.sh
+++ start_vim.sh
@@ -4,44 +4,91 @@
 # Author: meetbill
 # mail: meetbill@163.com
 # Created Time: Thu 06 Nov 2014 06:31:50 PM CST
+# Updated Time: 2018/03/04 16:24
 #########################################################################
 #Define Path    
 VIMRC=~/.vimrc 
 VIM_FILE=./packages/vim*
 
+function scripts_generic_identifyOs(){
+
+    ## determine OS of computer
+
+    os=$(uname -a)
+    if [[ ${os} == *"Darwin"* ]]; then
+      os="Mac"
+    elif [[ ${os} == *"Ubuntu"* ]]; then
+      os="Ubuntu"
+    elif [[ "$(cat /etc/system-release-cpe)" == *"centos"* ]]; then
+      os="Centos"
+    elif [[ "$(cat /etc/system-release-cpe)" == *"redhat"* ]]; then
+      os="Redhat"
+    else
+      os="Unrecognised"
+    fi
+}
 #deine Vim_config     
 function Vim_config ()      
 {   
 	clear
+    
+
     if [ `id -u` -eq 0 ];
     then
 	    tar -zxf ${VIM_FILE} -C ~/
-	    read -p "Please input your name:" AUTHOR      
-	    sed -i "s/Bill/$AUTHOR/g" $VIMRC     
-	    read -p "Please input your E-mail:" MAIL_AUTHOR     
-	    sed -i "s/XXXXXXX@qq.com/$MAIL_AUTHOR/g" $VIMRC     
+        if [[ "w$os" == "wMac" ]]
+        then
+            read -p "Please input your name:" AUTHOR      
+            sed -i "" "s/Bill/$AUTHOR/g" $VIMRC     
+            read -p "Please input your E-mail:" MAIL_AUTHOR     
+            sed -i "" "s/XXXXXXX@qq.com/$MAIL_AUTHOR/g" $VIMRC     
+        else
+            read -p "Please input your name:" AUTHOR      
+            sed -i "s/Bill/$AUTHOR/g" $VIMRC     
+            read -p "Please input your E-mail:" MAIL_AUTHOR     
+            sed -i "s/XXXXXXX@qq.com/$MAIL_AUTHOR/g" $VIMRC     
+        fi
 	    cd ./packages/
-        tar -zxf ctags.tar.gz -C /usr/bin/
-        chmod 755 /usr/bin/ctags
-	    CK_VIM=`grep "vi='vim'" ~/.bashrc | wc -l`
-	    if [ "w${CK_VIM}" = "w0" ]
-	    then
-		    echo " " >> ~/.bashrc
-		    echo "alias vi='vim'" >> ~/.bashrc
-	    fi 
-	    . ~/.bashrc
-	    echo "this vim config is success !" 
+        check_ctags=`ls -l /usr/bin/ | grep ctags|wc -l`
+        if [[ "w$check_ctags" == "w0" ]]
+        then
+            tar -zxf ctags.tar.gz -C /usr/bin/
+            chmod 755 /usr/bin/ctags
+        fi
+        if [[ -e ~/.bashrc  ]]
+        then
+            CK_VIM=`grep "vi='vim'" ~/.bashrc | wc -l`
+            if [ "w${CK_VIM}" = "w0" ]
+            then
+                echo " " >> ~/.bashrc
+                echo "alias vi='vim'" >> ~/.bashrc
+            fi 
+            . ~/.bashrc
+        fi
+        echo "this vim config is success !" 
 	    exit 0
     else
-        chmod 777 -R *
-	    sudo tar -zxf ${VIM_FILE} -C ~/
-	    read -p "Please input your name:" AUTHOR      
-	    sed -i "s/Bill/$AUTHOR/g" $VIMRC     
-	    read -p "Please input your E-mail:" MAIL_AUTHOR     
-	    sed -i "s/XXXXXXX@qq.com/$MAIL_AUTHOR/g" $VIMRC     
+        chmod -R 777 packages
+	    tar -zxf ${VIM_FILE} -C ~/
+        if [[ "w$os" == "wMac" ]]
+        then
+            read -p "Please input your name:" AUTHOR      
+            sed -i "" "s/Bill/$AUTHOR/g" $VIMRC     
+            read -p "Please input your E-mail:" MAIL_AUTHOR     
+            sed -i "" "s/XXXXXXX@qq.com/$MAIL_AUTHOR/g" $VIMRC     
+        else
+            read -p "Please input your name:" AUTHOR      
+            sed -i "s/Bill/$AUTHOR/g" $VIMRC     
+            read -p "Please input your E-mail:" MAIL_AUTHOR     
+            sed -i "s/XXXXXXX@qq.com/$MAIL_AUTHOR/g" $VIMRC     
+        fi
 	    cd ./packages/
-        sudo tar -zxf ctags.tar.gz -C /usr/bin/
-        sudo chmod 755 /usr/bin/ctags
+        check_ctags=`ls -l /usr/bin/ | grep ctags|wc -l`
+        if [[ "w$check_ctags" == "w0" ]]
+        then
+            sudo tar -zxf ctags.tar.gz -C /usr/bin/
+            sudo chmod 755 /usr/bin/ctags
+        fi
         if [[ -e ~/.bashrc  ]]
         then
             CK_VIM=`grep "vi='vim'" ~/.bashrc | wc -l`
@@ -74,6 +121,10 @@ VIM_PATH=$(cd `dirname $0`; pwd)
 cd ${VIM_PATH}
 
 
+scripts_generic_identifyOs
+echo "OS:"${os}
+
+echo " "
 PS3="Please input a number":              
   select i in  "Vim_config" "quit"
 do 
